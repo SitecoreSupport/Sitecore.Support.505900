61be73c0eae925e491057a1b901a92e202bc0ba8
DK\dbr <dbr@sitecore.net>
2022-05-16 16:35:25 +0300

    Adding the solution files.

---
+++ src/Sitecore.Support.505900/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.505900")]
+[assembly: AssemblyProduct("Sitecore.Support.505900")]
+[assembly: ComVisible(false)]
